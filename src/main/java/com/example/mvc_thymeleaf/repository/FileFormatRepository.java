8bfc621e14d00324635dec7bc4e8df7d2658f8a1
AsPirat <vallis_nimbium@mail.ru>
2018-08-29 00:26:15 +0300

    Final version "Search system".

--- src/main/java/com/example/mvc_thymeleaf/repository/FileFormatRepository.java
+++ src/main/java/com/example/mvc_thymeleaf/repository/FileFormatRepository.java
@@ -4,4 +4,5 @@ import com.example.mvc_thymeleaf.entity.FileFormat;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FileFormatRepository extends JpaRepository<FileFormat, Long> {
+
 }
