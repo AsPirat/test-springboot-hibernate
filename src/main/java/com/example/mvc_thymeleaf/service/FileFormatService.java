33e82ec52339894508d014c14e467bb1773180fe
AsPirat <vallis_nimbium@mail.ru>
2018-08-29 00:25:44 +0300

    Final version "Search system".

--- src/main/java/com/example/mvc_thymeleaf/service/FileFormatService.java
+++ src/main/java/com/example/mvc_thymeleaf/service/FileFormatService.java
@@ -3,6 +3,7 @@ import com.example.mvc_thymeleaf.entity.FileFormat;
 import java.util.List;
 
 public interface FileFormatService {
+
     List<FileFormat> getAll();
     FileFormat getById(Long id);
 }
